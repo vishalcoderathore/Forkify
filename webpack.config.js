39b86ab3279b2a08d452901f5e79fc502b63b262
vishalcoderathore <vshl.rathore87@gmail.com>
2018-12-31 13:44:47 -0600

    added babel and polyfill support to convert >=es6 to es5

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,44 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry : './src/js/index.js',
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename : 'js/bundle.js'
+    // Entry point of the app 
+    //Polyfill converts js code (like promises or array.from()) to es5 that "implements" promise (since, promises don't exist in es5)
+    entry: [
+        'babel-polyfill', './src/js/index.js'
+    ],
+
+    // Destination of the bundled code in dist folder
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-    devServer : {
-        contentBase : './dist'
+
+    // Source folder required by the Dev Server to run the app
+    devServer: {
+        contentBase: './dist'
     },
-    plugins : [
+
+    // Plugin produces the distributable index.html file in dist folder
+    plugins: [
         new HTMLWebpackPlugin({
-            filename : 'index.html',
-            template : './src/index.html'          
+            filename: 'index.html', template: './src/index.html'
         })
-    ]
+    ],
+
+    // Babel configuration
+    module: {
+        rules: [
+            {
+                // Look for all files with .js extension
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
