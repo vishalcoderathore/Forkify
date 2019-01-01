af58c5d65892685dc43c3ebee07a75a5cc9c555c
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-01 00:30:52 -0600

    added loader animation while loading/displaying data

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base'
+import {elements, renderLoader, clearLoader} from './views/base'
 
 /* Global State of the App
  * - Search Object
@@ -21,11 +21,13 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
-        
+        renderLoader(elements.searchRes);
+
         // 4) Search for recipes
         await state.search.getResults();
          
         // 5) Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
