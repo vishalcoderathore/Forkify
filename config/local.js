4c5be2297537a5a07b2169210c28291d74c93148
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-01 23:31:52 -0600

    added key and postinstall script

--- config/local.js
+++ config/local.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_KEY : 'ADD YOUR KEY HERE',
+    API_KEY : '3f473546fe693a84617ef87ba2605040',
     API_URL : 'http://food2fork.com',
     CORS_PROXY : 'https://cors-anywhere.herokuapp.com/'
 };
