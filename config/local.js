7350ff462e43addf4131c60fdadcadb4a824a0a3
vishalcoderathore <vshl.rathore87@gmail.com>
2019-01-02 00:36:11 -0600

    added express server for heroku

--- config/local.js
+++ config/local.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_KEY : '3f473546fe693a84617ef87ba2605040',
+    API_KEY : '0bb3b9e859ec8f5de3c94bf46ac04fed',
     API_URL : 'http://food2fork.com',
     CORS_PROXY : 'https://cors-anywhere.herokuapp.com/'
 };
